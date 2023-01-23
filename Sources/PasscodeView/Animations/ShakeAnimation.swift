6c6388917da39e467285c2b2d5a48311fc4add68
nasircsms <47743849+nasircsms@users.noreply.github.com>
2023-01-23 18:19:06 +0400

    -- initial commit

---
+++ Sources/PasscodeView/Animations/ShakeAnimation.swift
@@ -0,0 +1,20 @@
+//
+//  ShakeAnimation.swift
+//  VisitorManagement
+//
+//  Created by Light on 20/01/2023.
+//
+
+import SwiftUI
+
+struct Shake: GeometryEffect {
+    var amount: CGFloat = 10
+    var shakesPerUnit = 3
+    var animatableData: CGFloat
+
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        ProjectionTransform(CGAffineTransform(translationX:
+            amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)),
+            y: 0))
+    }
+}
--- Sources/PasscodeView/PasscodeView.swift
+++ Sources/PasscodeView/PasscodeView.swift
@@ -1,6 +1,67 @@
-public struct PasscodeView {
-    public private(set) var text = "Hello, World!"
+//
+//  PasscodeView.swift
+//  SwiftUIPasscode
+//
+//  Created by Light on 23/01/2023.
+//  
+//
 
-    public init() {
+import SwiftUI
+
+public struct PasscodeView {
+
+    // MARK: - Properties
+
+    @State private var passcodeInput = PasscodeInput()
+    @State var loginAttempts: Int = 0
+
+    public var passcodeValidationCallback: ((String) -> Bool)
+}
+
+// MARK: - View
+
+extension PasscodeView: View {
+
+    public var body: some View {
+        VStack(spacing: 40.0) {
+            PasscodeInputIndicatorView(PasscodeInput: $passcodeInput)
+                .modifier(Shake(animatableData: CGFloat(loginAttempts)))
+                .onChange(of: passcodeInput.isComplete) { isComplete in
+                    if isComplete {
+                        let isValid = passcodeValidationCallback(passcodeInput.input)
+                        if !isValid {
+                            invalidPasscode()
+                        }
+                    }
+                }
+            PasscodeKeyboardView(PasscodeInput: $passcodeInput)
+            HStack {
+                Button(action: {
+                    withAnimation {
+                        self.eraseOrCancel()
+                    }
+                }, label: {
+                    Text("Clear")
+                })
+            }
+        }
+        .preferredColorScheme(.dark)
+    }
+
+}
+
+// MARK: - Private methods
+
+extension PasscodeView {
+    
+    private func invalidPasscode() {
+        withAnimation {
+            self.loginAttempts += 1
+            self.eraseOrCancel()
+        }
+    }
+    
+    private func eraseOrCancel() {
+        passcodeInput.clear()
     }
 }
